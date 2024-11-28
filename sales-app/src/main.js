b5d423f37ed5baf54a16eee3a183378e9335ab82
Bakora2001 <maxwellbakora@gmail.com>
2024-11-29 01:34:29 +0300

    Added the app'structure layout with the possile imports in main.js

--- sales-app/src/main.js
+++ sales-app/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import Vue from 'vue';
+import router from '.router';
+import store from './store';
+import './style.css';
+import App from './App.vue';
+// I am Creating a new Vue instance that i'll mount it to the DOM
+new Vue({
+    router, // puts the router into the Vue instance
+    store, 
+    render: h => h(App), // Rendering the root App component
+  }).$mount('#app'); // Mounting the Vue instance to the DOM
 
-createApp(App).mount('#app')
